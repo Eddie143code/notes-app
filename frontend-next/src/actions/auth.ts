954088ae3c4f9c5336678a2b9f97b7f56cc5ec05
eddie143code <eddie225561@gmail.com>
2024-09-25 19:58:27 +0200

    fix

--- frontend-next/src/actions/auth.ts
+++ frontend-next/src/actions/auth.ts
@@ -1,33 +1,4 @@
 "use server"
-import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
-
-export const getCookies = async () => {
-    const cookieStore = cookies();
-    const userEmail = cookieStore.get("userEmail")?.value;
-  
-    if (!userEmail) {
-      // Use Next.js server-side redirect
-      redirect("/auth/login");
-    }
-  
-    const response = await fetch("http://localhost:3001/user/verify", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: userEmail }), // Send email in the request body
-      credentials: "include", // Send cookies with the request
-    });
-  
-    const result = await response.json();
-    if (!response.ok) {
-      redirect("/auth/login");
-    }
-  
-    return result; // Return result for further use if needed
-  };
-
 export const signup = async () => {
     const payload = JSON.stringify({
         email: "z@example.com",
@@ -46,25 +17,7 @@ export const signup = async () => {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
-        const data = await response.json(); // Corrected method name
-        console.log(data); // Log the parsed JSON data
     } catch (error) {
         console.error('Error:', error);
     }
 };
-
-
-// export const sigin = async () => {
-//     const payload = JSON.stringify({
-//         email: "z@example.com",
-//     })
-
-// await fetch('http://localhost:3001/user/me', {
-//         method: "POST",
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: payload,
-//     })
-// }
--- frontend-next/src/middleware.ts
+++ frontend-next/src/middleware.ts
@@ -1,33 +1,3 @@
-import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
-
-// export const getCookies = async () => {
-//     const cookieStore = cookies();
-//     const userEmail = cookieStore.get("userEmail")?.value;
-  
-//     if (!userEmail) {
-//       // Use Next.js server-side redirect
-//       redirect("/auth/login");
-//     }
-  
-//     const response = await fetch("http://localhost:3001/user/verify", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ email: userEmail }), // Send email in the request body
-//       credentials: "include", // Send cookies with the request
-//     });
-  
-//     const result = await response.json();
-//     if (!response.ok) {
-//       redirect("/auth/login");
-//     }
-  
-//     return result; // Return result for further use if needed
-//   };
-
-  // app/middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
@@ -61,11 +31,6 @@ export async function middleware(req: NextRequest) {
   // Continue with the request if authenticated
   return NextResponse.next();
 }
-
-// export function middleware(request: NextRequest) {
-//   console.log('in middleware')
-//   return NextResponse.redirect(new URL("/auth/login", request.url));
-// }
 
 export const config = {
   matcher: ['/'], // Apply middleware to all routes except those containing '/auth'
